1b6e379f8f1c1eee92189a96476ac4efee3485d0
Алиханов <138210056+alixonovshukurullo@users.noreply.github.com>
2024-01-31 16:50:25 +0500

    start

---
+++ rent-car.js
@@ -0,0 +1,29 @@
+const send = document.getElementById("send");
+const testdrive = document.querySelector(".rent-car"); // Исправлено на ".request-call"
+
+const data = [
+  {
+    img: "./Test drive/check 1.svg",
+    txta: "Отправлено",
+    txtb: "С вами свяжутся в ближайшее время",
+  },
+];
+
+// Функция для отображения окна с данными
+function showConfirmation() {
+  testdrive.innerHTML = ""; // Очистить содержимое формы
+  data.forEach((item) => {
+    testdrive.innerHTML += `
+              <div class="sendpage">
+                  <img src="${item.img}" alt="Галочка">
+                  <p>${item.txta}</p>
+                  <p>${item.txtb}</p>
+              </div>
+          `;
+  });
+}
+
+send.addEventListener("click", (event) => {
+  event.preventDefault(); // Отменить стандартное поведение кнопки submit
+  showConfirmation(); // Вызвать функцию для отображения окна с данными
+});
